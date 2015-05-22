8c35bf5067097286c510bb6d41f6afd9084aa8dc
globalprogramming <globalprogramming@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-22 10:24:51 +0000

    new version trunk submission
    git-svn-id: https://plugins.svn.wordpress.org/whmcs-bridge/tags/3.2.1@1165596 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pages/help (1).php
@@ -0,0 +1,16 @@
+<h2>The Bridge Page</h2>
+<p>A WHMCS front end page has been created on your WordPress site.
+    This page is the main interaction page between Wordpress and WHMCS.</p>
+<p>The full url is:
+    <a href="<?php echo cc_whmcs_bridge_home($home,$pid);?>">
+        <code>
+            <?php echo cc_whmcs_bridge_home($home,$pid);?>
+        </code>
+    </a>.
+    You can edit the page link by editing the page and changing the permalink.</p>
+    <p style="color:red"><strong>Do not delete this page!</strong></p>
+
+<?php
+if (file_exists(dirname(__FILE__).'/../../whmcs-bridge-sso/pages/help.php'))
+    require(dirname(__FILE__).'/../../whmcs-bridge-sso/pages/help.php');
+?>
